26cca5ee910e2d47a5028fe65a1bc22b53d83af4
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-16 14:09:15 +0000

    add dodger blue colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
             fontFamily: {
                 inter: ['Inter', 'sans-serif'],
             },
+            colors: {
+                'dodger-blue': {
+                    100: '#1da1f2',
+                    90: '#34aaf3',
+                    80: '#4ab4f5',
+                    70: '#61bdf6',
+                    60: '#77c7f7',
+                    50: '#8ed0f9',
+                    40: '#a5d9fa',
+                    30: '#bbe3fb',
+                    20: '#d2ecfc',
+                    10: '#e8f6fe',
+                },
+            },
         },
     },
     plugins: [],
